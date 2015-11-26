2a419ec258183696e6711b77a087ed3c46342945
Mike <mikey@phantomelite.com>
2015-11-26 10:17:37 -0500

    fixed edit, checking for instanceof

--- EditSemester.java
+++ EditSemester.java
@@ -9,6 +9,9 @@ import javax.swing.JTextField;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import baseclass.Semester;
 
 public class EditSemester extends JFrame {
 
@@ -27,10 +30,30 @@ public class EditSemester extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+		JComboBox cmbSeason = new JComboBox(new String[] { "", "Spring", "Summer", "Fall", "Winter" } );
+		cmbSeason.setBounds(81, 51, 127, 20);
+		contentPane.add(cmbSeason);
+		
 		JComboBox cmbSemesters = new JComboBox();
+		cmbSemesters.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (cmbSemesters.getSelectedItem() instanceof Semester)
+				{
+					Semester s = (Semester)cmbSemesters.getSelectedItem();
+					txtYear.setText(Integer.toString(s.getYear()));
+					cmbSeason.setSelectedItem(s.getSeasonName(((Semester)cmbSemesters.getSelectedItem()).getSeason()));
+				}
+				else
+				{
+					txtYear.setText("");
+					cmbSeason.setSelectedItem("");
+				}
+			}
+		});
 		cmbSemesters.setModel(dcmSemester);
 		cmbSemesters.setBounds(356, 23, 127, 20);
 		contentPane.add(cmbSemesters);
+	
 		
 		JLabel lblSemesterName = new JLabel("Year:");
 		lblSemesterName.setBounds(10, 26, 113, 14);
@@ -38,18 +61,12 @@ public class EditSemester extends JFrame {
 		
 		txtYear = new JTextField();
 		txtYear.setBounds(81, 23, 127, 20);
-		txtYear.setText(Integer.toString(((Semester)cmbSemesters.getSelectedItem()).getYear()));
 		contentPane.add(txtYear);
 		txtYear.setColumns(10);
 		
 		JButton btnSubmit = new JButton("Submit");
 		btnSubmit.setBounds(119, 82, 89, 23);
 		contentPane.add(btnSubmit);
-		
-		JComboBox cmbSeason = new JComboBox(new String[] { "", "Spring", "Summer", "Fall", "Winter" } );
-		cmbSeason.setSelectedItem(((Semester)cmbSemesters.getSelectedItem()).getSeason().toString());
-		cmbSeason.setBounds(81, 51, 127, 20);
-		contentPane.add(cmbSeason);
 		
 		JLabel lblNewLabel = new JLabel("Season:");
 		lblNewLabel.setBounds(10, 51, 57, 14);
@@ -58,11 +75,22 @@ public class EditSemester extends JFrame {
 		JLabel lblSemesterEditForm = new JLabel("Semester edit form");
 		lblSemesterEditForm.setBounds(10, 1, 113, 14);
 		contentPane.add(lblSemesterEditForm);
-		
-
-		
+				
 		JLabel lblSemesterToEdit = new JLabel("Semester to edit:");
 		lblSemesterToEdit.setBounds(245, 26, 103, 14);
 		contentPane.add(lblSemesterToEdit);
+		
+		if (cmbSemesters.getSelectedItem() instanceof Semester)
+		{
+			Semester s = (Semester)cmbSemesters.getSelectedItem();
+			System.out.println(s);
+			txtYear.setText(Integer.toString(s.getYear()));
+			cmbSeason.setSelectedItem(s.getSeasonName(((Semester)cmbSemesters.getSelectedItem()).getSeason()));
+		}
+		else
+		{
+			txtYear.setText("");
+			cmbSeason.setSelectedItem("");
+		}
 	}
 }
