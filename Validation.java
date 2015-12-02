a8878857abda6bfad20a9d72c7ada9a87ef98996
Mike <mikey@phantomelite.com>
2015-12-02 12:24:03 -0500

    Added javadoc to validation

--- Validation.java
+++ Validation.java
@@ -1,5 +1,10 @@
 
 public class Validation {
+	/**
+	 * Validates whether something is a double or not
+	 * @param str takes a string to use a parse function to check whether if valid or not
+	 * @return true if valid, false otherwise
+	 */
 	public static boolean isDouble(String str) {
 	    try {
 	        Double.parseDouble(str);
@@ -8,9 +13,14 @@ public class Validation {
 	        return false;
 	    }
 	}
-	public static boolean isInteger(String s) {
+	/**
+	 * 
+	 * @param str takes a string and uses the Integer parse function to check whether if string is a valid int
+	 * @return true if valid, false otherwise
+	 */
+	public static boolean isInteger(String str) {
 		try { 
-			Integer.parseInt(s); 
+			Integer.parseInt(str); 
 			return true;
 		} catch(NumberFormatException e) { 
 			return false; 
